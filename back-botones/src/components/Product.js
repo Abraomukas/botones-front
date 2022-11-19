f681092e264aeb1f3ba735b8080858e5b70b824d
Abraomukas <abraomukas@gmail.com>
2022-11-19 17:27:08 +0400

    feat: add card footer

--- back-botones/src/components/Product.js
+++ back-botones/src/components/Product.js
@@ -19,6 +19,27 @@ export default class Product extends Component {
 						<Link to='/details'>
 							<img src={img} alt='Product' className='card-img-top' />
 						</Link>
+						<button
+							className='cart-btn'
+							disabled={inCart ? true : false}
+							onClick={() => {
+								console.log('Added to the cart!');
+							}}>
+							{inCart ? (
+								<p className='text-capitalize mb-0' disabled>
+									in cart
+								</p>
+							) : (
+								<i className='fas fa-cart-plus' />
+							)}
+						</button>
+					</div>
+					<div className='card-footer d-flex justify-content-between'>
+						<p className='align-self-center mb-0'>{title}</p>
+						<h5 className='text-blue font-italic mb-0'>
+							<span className='mr-1'>$</span>
+							{price}
+						</h5>
 					</div>
 				</div>
 				<h3>Coming soon...</h3>
