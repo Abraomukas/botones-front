15260a274e969d566061762b92bc171464912829
Abraomukas <abraomukas@gmail.com>
2022-11-21 21:01:14 +0400

    feat: add styles to the product card
    Fixed transitions!

--- back-botones/src/components/Product.js
+++ back-botones/src/components/Product.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 
 import { ProductConsumer } from '../context';
 
@@ -12,7 +12,7 @@ export default class Product extends Component {
 			<ProductWrapper className='col-9 mx-auto col-md-6 col-lg-3 my-3'>
 				<div className='card'>
 					<div
-						className='p-5'
+						className='img-container p-5'
 						onClick={() => {
 							console.log('Clicked on the image container!');
 						}}>
@@ -34,6 +34,7 @@ export default class Product extends Component {
 							)}
 						</button>
 					</div>
+					{/* Card footer */}
 					<div className='card-footer d-flex justify-content-between'>
 						<p className='align-self-center mb-0'>{title}</p>
 						<h5 className='text-blue font-italic mb-0'>
@@ -42,38 +43,66 @@ export default class Product extends Component {
 						</h5>
 					</div>
 				</div>
-				<h3>Coming soon...</h3>
 			</ProductWrapper>
 		);
 	}
 }
 
-const ProductWrapper = Styled.div`
-.card{
-	border-color:transparent;
-	transition: all 1s linear;
-}
-.card-footer {
-	background:transparent;
-	border-top:transparent;
-}
-&:hover {
+const ProductWrapper = styled.div`
 	.card {
-		border:0.04rem solid rgba(0,0,0,0.2);
-		box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+		border-color: transparent;
+		transition: all 1s linear;
 	}
+
 	.card-footer {
-		background:rgba(247,247,247);
+		background: transparent;
+		border-top: transparent;
+		transition: all 1s linear;
 	}
-}
-.img-container {
-	position:relative;
-	overflow:hidden;
-}
-.card-img-top {
-	transition: all 1s linear;
-}
-.img-container:hover .card-img-top {
-	transform: scale(1.2);
-}
+
+	&:hover {
+		.card {
+			border: 0.04rem solid rgba(0, 0, 0, 0.2);
+			box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+		}
+		.card-footer {
+			background: rgba(247, 247, 247);
+		}
+	}
+
+	.img-container {
+		position: relative;
+		overflow: hidden;
+	}
+
+	.img-container:hover .card-img-top {
+		transform: scale(1.2);
+	}
+
+	.card-img-top {
+		transition: all 0.5s linear;
+	}
+
+	.cart-btn {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		padding: 0.2rem 0.4rem;
+		background: var(--lightBlue);
+		border: none;
+		color: var(--mainWhite);
+		font-size: 1.4rem;
+		border-radius: 0.5rem 0 0 0;
+		transform: translate(100%, 100%);
+		transition: all 0.5s linear;
+	}
+
+	.img-container:hover .cart-btn {
+		transform: translate(0, 0);
+	}
+
+	.cart-btn:hover {
+		color: var(--mainBlue);
+		cursor: pointer;
+	}
 `;
