6ddb8a7a1ceffeb06bbff4f2815091db312b6224
Abraomukas <abraomukas@gmail.com>
2022-11-24 14:39:50 +0400

    feat: finish ProductDetails skeleton
    Rendered all visual elements + Added a separate Button file

--- back-botones/src/routes/ProductDetails.js
+++ back-botones/src/routes/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ButtonContainer } from '../components/Button';
 import Navbar from '../components/Navbar';
 
 import { ProductConsumer } from '../context';
@@ -22,6 +23,36 @@ export default class ProductDetails extends Component {
 									<div className='row'>
 										<div className='col-10 mx-auto text-center text-blue my-5'>
 											<h1>{title}</h1>
+										</div>
+									</div>
+									{/* Details */}
+									<div className='row'>
+										<div className='col-10 mx-auto col-md-6 my-3'>
+											{/* Picture */}
+											<img src={img} className='img-fluid' alt={title} />
+										</div>
+										<div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+											{/* Name */}
+											<h2>model: {title}</h2>
+											{/* Price */}
+											<h4 className='text-blue'>
+												<strong>
+													price: <span>$</span>
+													{price}
+												</strong>
+											</h4>
+											{/* Info */}
+											<p className='text-capitalize font-weight-*bold mt-3 mb-0'>
+												info:
+											</p>
+											<p className='text-muted lead'>{info}</p>
+											{/* Buttons */}
+											<Link to='/products'>
+												<ButtonContainer>back to products</ButtonContainer>
+												<ButtonContainer disabled={inCart ? true : false}>
+													{inCart ? 'in the cart already' : 'add to cart'}
+												</ButtonContainer>
+											</Link>
 										</div>
 									</div>
 								</div>
