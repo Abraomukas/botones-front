1a2d41b2726bbb60c4121311ee130db7eda93130
Abraomukas <abraomukas@gmail.com>
2022-12-05 15:40:42 +0400

    feat: add new field to Item.js
    add trash icon to remove an item

--- back-botones/src/components/cart/Item.js
+++ back-botones/src/components/cart/Item.js
@@ -5,7 +5,8 @@ export default function Item({ item, product }) {
 	const { increment, decrement, removeItem } = product;
 
 	return (
-		<div className='row my-1 text-capitalize text-center'>
+		<div className='row my-2 text-capitalize text-center'>
+			{/* Image */}
 			<div className='col-10 mx-auto col-lg-2'>
 				<img
 					src={img}
@@ -14,14 +15,17 @@ export default function Item({ item, product }) {
 					alt='product'
 				/>
 			</div>
+			{/* Title */}
 			<div className='col-10 mx-auto col-lg-2'>
 				<span className='d-lg-none'>product: </span>
 				{title}
 			</div>
+			{/* Price */}
 			<div className='col-10 mx-auto col-lg-2'>
 				<span className='d-lg-none'>price: </span>
 				{price}
 			</div>
+			{/* Buttons */}
 			<div className='col-10 mx-auto col-lg-2 my-2 my-lg-0'>
 				<div className='d-flex justify-content-center'>
 					<div>
@@ -41,6 +45,16 @@ export default function Item({ item, product }) {
 							+
 						</span>
 					</div>
+				</div>
+			</div>
+			{/* Remove item */}
+			<div className='col-10 mx-auto co-lg-2'>
+				<div
+					className='cart-icon'
+					onClick={() => {
+						removeItem(id);
+					}}>
+					<i className='fas fa-trash'></i>
 				</div>
 			</div>
 		</div>
