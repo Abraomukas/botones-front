f3efa735f7e30bde8f044f682d258277a80dd24f
Abraomukas <abraomukas@gmail.com>
2022-12-08 12:42:39 +0400

    feat: fix items display

--- back-botones/src/components/cart/List.js
+++ back-botones/src/components/cart/List.js
@@ -3,12 +3,10 @@ import Item from './Item';
 
 export default function List({ data }) {
 	const { cart } = data;
-	console.log("D - ", data);
-	console.log("C - ", cart);
 
 	return (
 		<div className='container-fluid'>
-			{data.products.map((item) => {
+			{cart.map((item) => {
 				return <Item key={item.id} item={item} product={data} />;
 			})}
 		</div>
